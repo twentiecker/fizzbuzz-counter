fd7d641c5eb42fa6708363b364c4bfd12ff4e8c5
twentiecker <twentiecker@gmail.com>
2022-06-21 14:54:30 +0700

    add CounterApp

---
+++ src/components/CounterApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CounterApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>TODO</p>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
