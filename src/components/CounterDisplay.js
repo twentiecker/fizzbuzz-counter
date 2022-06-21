74a3e7929a83da594a59ea8aea2c595fdb4eb55b
twentiecker <twentiecker@gmail.com>
2022-06-21 16:00:09 +0700

    add fizzbuzz case

--- src/components/CounterDisplay.js
+++ src/components/CounterDisplay.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function CounterDisplay({ count }) {
-  return <p>{count} </p>;
+  if (count === 0) {
+    return <p>{count} </p>;
+  } else if (count % 5 === 0 && count % 7 === 0) {
+    return <p>FizzBuzz </p>;
+  } else if (count % 5 === 0) {
+    return <p>Fizz </p>;
+  } else if (count % 7 === 0) {
+    return <p>Buzz </p>;
+  } else {
+    return <p>{count} </p>;
+  }
 }
 
 export default CounterDisplay;
